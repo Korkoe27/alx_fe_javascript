87c0c7ef07209a16342d4c0406c36c1b4cb33676
Mechanic <dumashieanthony@gmail.com>
2025-06-29 07:07:51 +0000

    instantiated project

---
+++ dom-manipulation/script.js
@@ -0,0 +1,32 @@
+
+
+
+let qoutes = [
+
+        {
+            "text":"The greatest glory in living lies not in never falling, but in rising every time we fall.",
+            "category":"inspirational"
+        },
+        {
+            "text":"The way to get started is to quit talking and begin doing.",
+            "category":"motivational"
+        },
+        {
+            "text":"Life is what happens when you're busy making other plans.",
+            "category":"life"
+        },
+        {
+            "text":"Get busy living or get busy dying.",
+            "category":"life"
+        },
+        {
+            "text":"You only live once, but if you do it right, once is enough.",
+            "category":"life"
+        }
+
+    ]
+
+
+function showRandomQuote(){
+
+}
